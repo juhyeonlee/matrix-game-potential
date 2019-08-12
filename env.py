3bba41b5d55dbddfc22c3afe889c0f6d4dfef4d1
Juhyeon Lee <jhlee899@gmail.com>
2019-08-13 02:10:38 +0900

    two step matrix game for global controller

--- env.py
+++ env.py
@@ -4,26 +4,46 @@ import numpy as np
 # from QTRAN code
 class MultiAgentSimpleEnv2(gym.Env):  # Matrix game
     def __init__(self):
-        self.state = [1]
+        self.state_set = np.eye(3) # state 1, 2A, 2B
+        self.state = 0
         self.action_dim = 3
-        self.state_dim = 1
+        self.state_dim = 3
 
-        self.payoff2 = np.array([[8., -12., -12.], [-12., 0., 0.], [-12., 0., 0.]])
-
+        self.payoff_A = np.array([[4., 4., 4.], [4., 4., 4.], [4., 4., 4.]])
+        self.payoff_B = np.array([[8., 5., 2.], [5., 2., 1.], [2., 1., 0.]])
     def reset(self):
-        self.state = [1]
+        self.state = 0
+        state = self.state_set[self.state]
 
-        return self.state
+        return state
 
     def step(self, action):
         info = {'n': []}
         reward = []
         done = []
-        reward.append(self.payoff2[action[0], action[1]])
-        self.state = [3]
-        done.append(True)
+        if self.state == 0:
+            if action[0] == 1 or action[0] == 2:
+                next_state = 1
+            elif action[0] == 0:
+                next_state = 2
+            reward.append(0)
+            done.append(False)
 
-        return self.state, reward, done, info
+        elif self.state == 1:
+            next_state = 0
+            reward.append(self.payoff_A[action[0]][action[1]])
+            done.append(True)
+
+        elif self.state == 2:
+            next_state = 0
+            reward.append(self.payoff_B[action[0]][action[1]])
+            done.append(True)
+
+        self.state = next_state
+        next_state_out = self.state_set[next_state]
+
+
+        return next_state_out, reward, done, info
 
     def call_action_dim(self):
         return self.action_dim
--- globalq.py
+++ globalq.py
@@ -14,28 +14,38 @@ class GlobalQ(nn.Module):
         self.batch_size = args['batch_size']
         self.gamma = args['discount_factor']
         self.global_lr = args['global_lr']
-        self.q_table = np.zeros((self.n_actions, self.n_actions), dtype=np.float)
+        self.n_states = args['n_states']
+        self.q_table = np.zeros((self.n_states, self.n_actions, self.n_actions), dtype=np.float)
 
-
-    def forward(self, batch):
+    def forward(self, batch_state, batch_actions):
         q = []
         for idx in range(self.batch_size):
-            a = batch[idx]
-            q.append(self.q_table[a[0]][a[1]])
+            s = batch_state[idx]
+            s_idx = torch.argmax(s).item()
+            a = batch_actions[idx]
+            q.append(self.q_table[s_idx][a[0]][a[1]])
         q = torch.tensor(q)
         return q
 
     def learn(self, batch):
+        batch_state = torch.from_numpy(np.array(list(batch[:, 0])))
         batch_action = torch.from_numpy(np.array(list(batch[:, 1])))
         batch_reward = torch.from_numpy(np.array(list(batch[:, 2])))
+        batch_state_n = torch.from_numpy(np.array(list(batch[:, 3])))
+        batch_terminated = torch.from_numpy(np.array(list(batch[:, 4]))).type(torch.float32)  # done
 
         for idx in range(self.batch_size):
+            s = batch_state[idx]
+            s_idx = torch.argmax(s).item()
+            s_n = batch_state_n[idx]
+            s_n_idx = torch.argmax(s_n).item()
             a = batch_action[idx]
-            new_q = batch_reward[idx] #+ self.gamma * max(self.q_table[next_state])
-            self.q_table[a[0]][a[1]] += self.global_lr * (new_q - self.q_table[a[0]][a[1]])
+            new_q = batch_reward[idx] + self.gamma * (1 - batch_terminated[idx]) * np.max(self.q_table[s_n_idx])
+            self.q_table[s_idx][a[0]][a[1]] += self.global_lr * (new_q - self.q_table[s_idx][a[0]][a[1]])
 
-    def select_max_action(self):
-        a = np.unravel_index(np.argmax(self.q_table, axis=None), self.q_table.shape)
+    def select_max_action(self, batch_state):
+        s = torch.argmax(batch_state).item()
+        a = np.unravel_index(np.argmax(self.q_table[s], axis=None), self.q_table.shape)
 
         return a
     # def init_hidden(self):
